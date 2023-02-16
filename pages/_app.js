d8dcedac295fe5ff0d4811c9fed5e1c45e3b6285
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-02-16 16:47:50 +0100

    add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="bg-creme grid h-screen place-items-center">
+      <Head>
+        <title>Perfume by Gabrielle Essence</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Perfume by Gabrielle Essence</title>
+        <meta
+          property="og:title"
+          content="Perfume by Gabrielle Essence"
+          key="title"
+        />
         <link
           link
           href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,700&family=Montserrat:wght@500;700&display=swap"
